6c7b9253a0bf87b34553dd943027388086dbd18f
EstebanCano210 <ecano-2020374@kinal.edu.gt>
2025-04-22 11:52:13 -0600

    Correcion de errores settings

---
+++ src/shared/hooks/useFollowChannel.jsx
@@ -0,0 +1,23 @@
+import toast from "react-hot-toast";
+import { followChannel as followChannelRequest } from '../../services'
+
+export const useFollowChannel = () => {
+
+    const followChannel = async (channelId, onSucces) => {
+
+        const responseData = await followChannelRequest(channelId)
+
+        if (responseData.error) {
+            return toast.error(
+                responseData.e?.response?.data || 'Ocurrio un error al seguir al canal'
+            )
+        }
+
+        toast.success('channel followed successfully!')
+        onSucces(true)
+    }
+    
+    return{
+        followChannel
+    }
+}
